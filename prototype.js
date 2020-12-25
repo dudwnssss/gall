44a58515d32eb32b9fbc93c467438233d952c457
dudwnssss <76581866+dudwnssss@users.noreply.github.com>
2020-12-25 17:12:18 +0900

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,15 @@
+var canvas = new fabric.Canvas('canvas');
+document.getElementById('file').addEventListener("change", function (e) {
+  var file = e.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function (f) {
+    var data = f.target.result;                    
+    fabric.Image.fromURL(data, function (img) {
+      var oImg = img.set({left: 0, top: 0, angle: 0,}).scale(0.5);
+      canvas.add(oImg).renderAll();
+      var a = canvas.setActiveObject(oImg);
+      var dataURL = canvas.toDataURL({format: 'png', quality: 0.8});
+    });
+  };
+  reader.readAsDataURL(file);
+});
